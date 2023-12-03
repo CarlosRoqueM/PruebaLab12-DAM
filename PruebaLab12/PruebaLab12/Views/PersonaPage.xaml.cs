dfc9e79cf8006ac13c35bd1d20b20b6c26e7f5ae
CarlosRoqueM <carlos.roque@tecsup.edu.pe>
2023-12-02 22:05:11 -0500

    Agregar archivos de proyecto.

---
+++ PruebaLab12/PruebaLab12/Views/PersonaPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using PruebaLab12.Model;
+using PruebaLab12.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PruebaLab12.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonaPage : ContentPage
+    {
+        PersonaViewModel contexto = new PersonaViewModel();
+        public PersonaPage()
+        {
+            InitializeComponent();
+            BindingContext = contexto;
+            LvPersonas.ItemSelected += LvPersonas_ItemSelected;
+        }
+
+        private void LvPersonas_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem != null)
+            {
+                PersonaModel modelo = (PersonaModel)e.SelectedItem;
+                contexto.Nombre = modelo.Nombre;
+                contexto.Apellido = modelo.Apellido;
+                contexto.Edad = modelo.Edad;
+                contexto.Id = modelo.Id;
+            }
+        }
+    }
+}
